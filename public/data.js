9c9596b88dae3d6540edb0fd823edf438c59aae8
DorianMoreno <dorian.r.mv@gmail.com>
2021-09-07 00:47:16 -0500

    init

---
+++ public/data.js
@@ -0,0 +1,94 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-prueba-360-definitivo",
+      "name": "PRUEBA 360 DEFINITIVO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.6269128464508498,
+        "pitch": 0.11012469547315717,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5110995339452593,
+          "pitch": 0.3169123583165039,
+          "rotation": 0,
+          "target": "1-prueba-360-definitivo-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-prueba-360-definitivo-2",
+      "name": "PRUEBA 360 DEFINITIVO 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4570554325785263,
+        "pitch": 0.11102620311957168,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.291824440866499,
+          "pitch": 0.31633753643867024,
+          "rotation": 0,
+          "target": "0-prueba-360-definitivo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7563594961459916,
+          "pitch": -0.1025777324219046,
+          "title": "Cajones de madera<br>",
+          "text": "Muy muy buenos<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
